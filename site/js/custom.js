e2c76732bd7bf9116a1c0de7b98e12bbf25fad58
ishg <ishmeetsinghgrewal@gmail.com>
2013-08-20 20:31:30 -0400

    Added Impossible List

--- site/js/custom.js
+++ site/js/custom.js
@@ -2,7 +2,13 @@
     $('#home-container h2, #home-container h3, #home-container p').hide();
     $('#home-container').css("width", "0px");
     $('#home-container').css("padding", "0px");
-    $('.switch').click(function () {
+    $('#home-container').delay(700).animate({
+                width: '800',
+                padding: '40'
+            });
+            $('#home-container h2, #home-container h3, #home-container p').delay(700).fadeIn(800);
+	
+	/*$('.switch').click(function () {
         var n = $('#home-container').width();
         if (n > 0) {
             $('#home-container h2, #home-container h3, #home-container p').fadeOut(350);
@@ -20,6 +26,6 @@
             $('#home-container h2, #home-container h3, #home-container p').fadeIn(800);
             $('.switch').text('CLOSE!');
         }
-    });
+    });*/
  
 });
